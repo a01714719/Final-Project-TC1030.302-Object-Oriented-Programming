a27c1120154dbf225214eee5e77211a3c25d7916
Sebastian Villegas Olaya <a01714719@tec.mx>
2026-06-11 00:40:27 -0600

    Update BankException.hpp

--- BankException.hpp
+++ BankException.hpp
@@ -1,1 +1,28 @@
-//tomorrow I will work on it 
+#ifndef BANK_EXCEPTION_HPP
+#define BANK_EXCEPTION_HPP
+
+#include <exception>
+#include <string>
+
+class BankException : public std::exception {
+public:
+    explicit BankException(const std::string& message) : message_(message) {}
+    const char* what() const noexcept override { return message_.c_str(); }
+
+private:
+    std::string message_;
+};
+
+class InsufficientFundsException : public BankException {
+public:
+    explicit InsufficientFundsException(const std::string& message)
+        : BankException(message) {}
+};
+
+class InvalidAmountException : public BankException {
+public:
+    explicit InvalidAmountException(const std::string& message)
+        : BankException(message) {}
+};
+
+#endif
