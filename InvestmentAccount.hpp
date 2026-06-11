7d1edaf9aba410561a5250064962f1f01b011bb5
Sebastian Villegas Olaya <a01714719@tec.mx>
2026-06-11 16:06:34 -0600

    Update InvestmentAccount.hpp

--- InvestmentAccount.hpp
+++ InvestmentAccount.hpp
@@ -1,1 +1,18 @@
-//mańana o pasado
+#ifndef INVESTMENT_ACCOUNT_HPP
+#define INVESTMENT_ACCOUNT_HPP
+
+#include "Account.hpp"
+
+class InvestmentAccount final : public Account {
+public:
+    InvestmentAccount(int id, const std::string& holderName,
+                      double initialBalance, double annualRate);
+
+    void applyInterest() override; 
+    std::string typeName() const override;
+
+private:
+    double annualRate_;
+};
+
+#endif
