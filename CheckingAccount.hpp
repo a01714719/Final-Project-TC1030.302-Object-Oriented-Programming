6f5040aa8e4de6244274386ced5af260f3cb9a5e
Sebastian Villegas Olaya <a01714719@tec.mx>
2026-06-11 10:23:33 -0600

    Update CheckingAccount.hpp

--- CheckingAccount.hpp
+++ CheckingAccount.hpp
@@ -1,1 +1,21 @@
-//mañana o pasadomañana? 
+#ifndef CHECKING_ACCOUNT_HPP
+#define CHECKING_ACCOUNT_HPP
+
+#include "Account.hpp"
+
+class CheckingAccount : public Account {
+public:
+    CheckingAccount(int id, const std::string& holderName,
+                    double initialBalance, double overdraftLimit);
+
+    void applyInterest() override;  
+    std::string typeName() const override;
+
+protected:
+    double minimumBalance() const override; 
+
+private:
+    double overdraftLimit_;
+};
+
+#endif
