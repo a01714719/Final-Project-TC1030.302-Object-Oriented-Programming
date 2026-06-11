6cb08d477fc4e34532f492c8b78570f802d0697f
Sebastian Villegas Olaya <a01714719@tec.mx>
2026-06-11 10:03:12 -0600

    Update SavingsAccount.hpp

--- SavingsAccount.hpp
+++ SavingsAccount.hpp
@@ -1,1 +1,22 @@
-//mañana 
+#ifndef SAVINGS_ACCOUNT_HPP
+#define SAVINGS_ACCOUNT_HPP
+
+#include "Account.hpp"
+
+class SavingsAccount : public Account {
+public:
+    SavingsAccount(int id, const std::string& holderName,
+                   double initialBalance, double interestRate,
+                   int monthlyWithdrawalLimit);
+
+    void withdraw(double amount) override;  
+    void applyInterest() override;          
+    std::string typeName() const override;
+
+private:
+    double interestRate_;
+    int monthlyWithdrawalLimit_;
+    int withdrawalsThisMonth_;
+};
+
+#endif
